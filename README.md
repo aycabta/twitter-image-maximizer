6458029a6a7a71d6f92f374aae0ae521747c9d53
Code Ass <aycabta@gmail.com>
2014-08-05 01:36:58 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Twitter Image Maximizer
+=======================
+
+[This Greasemonkey script on OpenUserJS.org](https://openuserjs.org/scripts/aycabta/Twitter_Image_Maximizer)
+
---
+++ twitter-image-maximizer.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name       Twitter Image Maximizer
+// @namespace  http://aycabta.github.io/
+// @version    0.1.0
+// @description  Show whole images on Twitter
+// @include    /^https:\/\/twitter.com
+// @copyright  2014+, Code Ass
+// ==/UserScript==
+
+(function() {
+    var sheet;
+    var style = document.createElement('style');
+    style.setAttribute('id', 'great-css');
+    style.type = "text/css";
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(style);
+    sheet = style.sheet;
+    sheet.insertRule('.TwitterPhoto-media { max-height: 100% !important; }', sheet.cssRules.length);
+    sheet.insertRule('.TwitterPhoto-mediaSource { margin-top: 0px !important; }', sheet.cssRules.length);
+    sheet.insertRule('.media-thumbnail { max-height: 100% !important; }', sheet.cssRules.length);
+    sheet.insertRule('.media-thumbnail img { margin-top: 0px !important; }', sheet.cssRules.length);
+})();
+
