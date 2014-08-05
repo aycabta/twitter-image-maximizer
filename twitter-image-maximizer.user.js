1a74e7d55b84da30289eaa71dce8f5491f63e08c
Code Ass <aycabta@gmail.com>
2014-08-05 17:13:34 +0900

    update to 0.1.2

--- twitter-image-maximizer.user.js
+++ twitter-image-maximizer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Twitter Image Maximizer
 // @namespace  http://aycabta.github.io/
-// @version    0.1.1
+// @version    0.1.2
 // @description  Show whole images on Twitter
 // @include    /^https:\/\/twitter.com
 // @copyright  2014+, Code Ass
