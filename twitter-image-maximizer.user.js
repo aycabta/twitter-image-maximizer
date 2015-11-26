93ed068ec52b566f558df185cb0919ff7c3ccf18
Code Ass <aycabta@gmail.com>
2015-11-26 22:52:31 +0900

    Support new Twitter DOM

--- twitter-image-maximizer.user.js
+++ twitter-image-maximizer.user.js
@@ -15,47 +15,30 @@
     var head = document.getElementsByTagName('head')[0];
     head.appendChild(style);
     sheet = style.sheet;
-    sheet.insertRule('.TwitterPhoto-media { max-height: 100% !important; }', sheet.cssRules.length);
-    sheet.insertRule('.TwitterPhoto-mediaSource { margin-top: 0px !important; }', sheet.cssRules.length);
-    sheet.insertRule('.media-thumbnail { max-height: 100% !important; }', sheet.cssRules.length);
-    sheet.insertRule('.media-thumbnail img { margin-top: 0px !important; }', sheet.cssRules.length);
-    sheet.insertRule('.FlexEmbed--2by1:before { padding-bottom: 0px !important; }', sheet.cssRules.length);
-    sheet.insertRule('.FlexEmbed-item { position: relative !important; }', sheet.cssRules.length);
-    sheet.insertRule('.TwitterMultiPhoto-image img {' +
-                     '    width: 100% !important;' +
+    sheet.insertRule('.permalink-tweet .OldMedia-quadPhoto, .permalink-tweet .OldMedia-triplePhoto, .permalink-tweet .OldMedia-doublePhoto {' +
                      '    height: auto !important;' +
-                     '}', sheet.cssRules.length);
-    sheet.insertRule('.TwitterMultiPhoto-image, .TwitterMultiPhoto-image img {' +
-                     '    position: relative !important;' +
-                     '    top: 0px !important;' +
-                     '    bottom: 0px !important;' +
-                     '    left: 0px !important;' +
-                     '    right: 0px !important;' +
-                     '}', sheet.cssRules.length);
-    sheet.insertRule('.multi-photos { height: auto !important;}', sheet.cssRules.length);
-    sheet.insertRule('.multi-photos .multi-photo {' +
-                     '    height: auto !important;' +
-                     '    width: auto !important;' +
-                     '    margin-right: 0px !important;' +
-                     '    margin-bottom: 6px !important;' +
-                     '}', sheet.cssRules.length);
-    sheet.insertRule('.multi-photos .multi-photo img {' +
-                     '    top: 0px !important;' +
-                     '    position: relative !important;' +
-                     '    left: 0 !important;' +
                      '    width: 100% !important;' +
+                     '}', sheet.cssRules.length);
+    sheet.insertRule('.OldMedia-photoContainer {' +
                      '    height: 100% !important;' +
-                     '}', sheet.cssRules.length);
-    sheet.insertRule('.QuoteTweet .multi-photo img {' +
-                     '    left: 0px !important;' +
                      '    width: 100% !important;' +
+                     '}', sheet.cssRules.length);
+    sheet.insertRule('.OldMedia-photoContainer img {' +
+                     '    position: static !important;' +
+                     '    width: 100% !important;' +
+                     '    margin-bottom: 7px !important;' +
+                     '}', sheet.cssRules.length);
+    sheet.insertRule('.OldMedia-halfPhoto, .OldMedia-quarterPhotoContainer, .OldMedia-quarterPhoto {' +
+                     '    width: auto !important;' +
+                     '    height: auto !important;' +
+                     '    margin-right: 0px !important;' +
+                     '    margin-left: 0px !important;' +
+                     '}', sheet.cssRules.length);
+    sheet.insertRule('.OldMedia-quadPhoto, .OldMedia-triplePhoto, .OldMedia-doublePhoto {' +
                      '    height: auto !important;' +
                      '}', sheet.cssRules.length);
-    sheet.insertRule('.QuoteTweet .multi-photos { width: 100% !important;}', sheet.cssRules.length);
-    sheet.insertRule('.u-tableCell { display: block !important;}', sheet.cssRules.length);
-    sheet.insertRule('.u-alignTop {' +
-                     '    vertical-align: baseline !important;' +
-                     '    display: inline-block !important;' +
+    sheet.insertRule('.OldMedia {' +
+                     '    max-height: none !important;' +
                      '}', sheet.cssRules.length);
 })();
 
